a08a5c457ea36d725a0e8854b93984dbce3f5ccb
Tom Minor <i7245143@bournemouth.ac.uk>
2016-02-23 13:25:01 +0000

    Node graph mostly works, restructured SDFOP layout

---
+++ Romanesco_Src/include/SDFOps/DistOp/Intersection_SDFOP.h
@@ -0,0 +1,15 @@
+
+#ifndef INTERSECTION_SDFOP_H 
+#define INTERSECTION_SDFOP_H 
+ 
+#include "DistOpInterface_SDFOP.h"
+ 
+class Intersection_SDFOP : public DistOpInterface_SDFOP
+{ 
+public: 
+    Intersection_SDFOP(); 
+    ~Intersection_SDFOP(); 
+}; 
+ 
+#endif // INTERSECTION_SDFOP_H 
+
---
+++ Romanesco_Src/include/SDFOps/DistOp/Subtraction_SDFOP.h
@@ -0,0 +1,15 @@
+
+#ifndef SUBTRACTION_SDFOP_H 
+#define SUBTRACTION_SDFOP_H 
+ 
+#include "DistOpInterface_SDFOP.h"
+ 
+class Subtraction_SDFOP : public DistOpInterface_SDFOP
+{ 
+public: 
+    Subtraction_SDFOP(); 
+    ~Subtraction_SDFOP(); 
+}; 
+ 
+#endif // SUBTRACTION_SDFOP_H 
+
